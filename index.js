5cd84e3c1dc6d1bc911ea9f94a6fad1126a66628
Nikita Finogenov <nikita.finogenov@gmail.com>
2024-08-19 16:32:03 +0300

    tommorowplus

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const apiKey = '408e224aa237a96f16800dbfd57490dc';
-const daysToShow = ;
+const daysToShow = 4;
 let cityName = "Kharkiv";
 
 const forecastElement = document.getElementById('forecast');
@@ -14,35 +14,39 @@ function getWeatherForecast(city) {
 }
 function displayWeatherForecast(weatherData) {
     const days = weatherData.list.slice(0, daysToShow);
-    const forecastHTML = days
-        .map(
-            (day) => `
-    <div class="day">
-      <h2>${new Date(day.dt * 1000).toLocaleDateString()}</h2>
-      <img
-        src="${(() => {
-                    if (day.weather[0].description.includes("sky")) {
-                        return "assets/images/sunny.png";
-                    } else if (day.weather[0].description.includes("cloud")) {
-                        return "assets/images/cloudy.png";
-                    } else if (day.weather[0].description.includes("rain")) {
-                        return "assets/images/rainy.png";
-                    }
-                    return "assets/images/default.png";
-                })()}"
-        alt="${day.weather[0].description}"
-      />
-      <p>${day.main.temp}°C</p>
-    </div>
-  `
-        )
-        .join('');
-    forecastElement.innerHTML = forecastHTML;
-    cityElement.innerHTML = `<h2>${cityName}</h2>`;
+//     const forecastHTML = days
+//         .map(
+//             (day) => `
+//     <div class="day">
+//       <h2>${new Date(day.dt * 1000).toLocaleDateString()}</h2>
+//       <img
+//         src="${(() => {
+//                     if (day.weather[0].description.includes("sky")) {
+//                         return "assets/images/sunny.png";
+//                     } else if (day.weather[0].description.includes("cloud")) {
+//                         return "assets/images/cloudy.png";
+//                     } else if (day.weather[0].description.includes("rain")) {
+//                         return "assets/images/rainy.png";
+//                     }
+//                     return "assets/images/default.png";
+//                 })()}"
+//         alt="${day.weather[0].description}"
+//       />
+//       <p>${day.main.temp}°C</p>
+//     </div>
+//   `
+//         )
+//         .join('');
+    // forecastElement.innerHTML = forecastHTML;
+    // cityElement.innerHTML = `<h2>${cityName}</h2>`;
+    document.getElementById("today").innerHTML = `${days[0].main.temp}°C`
+    document.getElementById("tomorrow").innerHTML = `${days[0].main.temp}°C`
+    document.getElementById("tomorrowplus").innerHTML = `${days[0].main.temp}°C`
+    document.getElementById("tomorrowplusplus").innerHTML = `${days[0].main.temp}°C`
 }
-function findWeather() {
-    cityName = document.getElementById("city-input").value;
-    getWeatherForecast(cityName);
-}
+// function findWeather() {
+//     cityName = document.getElementById("city-input").value;
+//     getWeatherForecast(cityName);
+// }
 
 getWeatherForecast(cityName);
